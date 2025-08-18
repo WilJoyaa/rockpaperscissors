055bba7918c376e111a40e0f2652c48842dfb429
WilJoyaa <wiljoyaa@gmail.com>
2025-08-17 22:28:05 -0400

    choice button logic
    added the logic to determine what user choice is depeding on which button is selected and need to add score counting logic

--- rpsgame.js
+++ rpsgame.js
@@ -9,76 +9,68 @@ function computerChoice (){
         return `scissors` ;
     }
 }
-function userChoice (){
-    let input = prompt("What will you choice be?")
-    input = input.toLowerCase();
-    return input;
+
+function playRound ( user,computer ){
+    if ( user == "rock" ){
+        if ( computer  == "rock" ){
+            console.log("Tie!");
+        }
+        if ( computer  == "paper" ){
+            computerScore ++;
+            console.log("Computer Wins This Round!");
+        }
+        if ( computer  == "scissors" ){
+            userScore ++;
+            console.log("You Win This Round!");
+        }
+    }
+    if ( user == "paper" ){
+        if ( computer  == "paper" ){
+            console.log("Tie!");
+        }
+        if ( computer  == "scissors" ){
+            computerScore ++;
+            console.log("Computer Wins This Round!");
+        }
+        if ( computer  == "rock" ){
+            userScore ++;
+            console.log("You Win This Round!");
+        }
+    }
+    if ( user == "scissors" ){
+        if ( computer  == "scissors" ){
+            console.log("Tie!");
+        }
+        if ( computer  == "rock" ){
+            computerScore ++;
+            console.log("Computer Wins This Round!");
+        }
+        if ( computer  == "paper" ){
+            userScore ++;
+            console.log("You Win This Round!");
+        }
+    }
 }
 
 
 
-function playGame (){
-    let userScore = 0;
-    let computerScore = 0;
+let option = document.querySelector(`#gamebtn`);
+option.addEventListener(`click`, (e) => {
+    let target = e.target;
 
-
-    function playRound ( user,computer ){
-        if ( user == "rock" ){
-            if ( computer  == "rock" ){
-                console.log("Tie!");
-            }
-            if ( computer  == "paper" ){
-                computerScore ++;
-                console.log("Computer Wins This Round!");
-            }
-            if ( computer  == "scissors" ){
-                userScore ++;
-                console.log("You Win This Round!");
-            }
-        }
-        if ( user == "paper" ){
-            if ( computer  == "paper" ){
-                console.log("Tie!");
-            }
-            if ( computer  == "scissors" ){
-                computerScore ++;
-                console.log("Computer Wins This Round!");
-            }
-            if ( computer  == "rock" ){
-                userScore ++;
-                console.log("You Win This Round!");
-            }
-        }
-        if ( user == "scissors" ){
-            if ( computer  == "scissors" ){
-                console.log("Tie!");
-            }
-            if ( computer  == "rock" ){
-                computerScore ++;
-                console.log("Computer Wins This Round!");
-            }
-            if ( computer  == "paper" ){
-                userScore ++;
-                console.log("You Win This Round!");
-            }
-        }
+    switch(target.id) {
+        case `rock`:
+            playRound ("rock",computerChoice());
+            break;
+        case `paper`:
+            playRound("paper",computerChoice());
+            break;
+        case `scissors`:
+            playRound("scissors",computerChoice());
     }
+});
 
 
-    for ( i = 0; i < 5; i++ ){
-        playRound(userChoice(), computerChoice());
-        console.log("Your score: " + userScore);
-        console.log("Computer score: " + computerScore);
-    }
 
 
-    if (userScore > computerScore){
-        console.log("You Win!");
-    } else if (userScore == computerScore){
-        console.log("Draw!")
-    }  else {
-        console.log("You Lose Try Again");
-    }
-}
 
-playGame();
