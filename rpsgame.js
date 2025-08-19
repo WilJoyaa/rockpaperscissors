65aeac23a865ee908b84ffc89574a731ddc596f1
WilJoyaa <wiljoyaa@gmail.com>
2025-08-18 20:53:25 -0400

    N/A

--- rpsgame.js
+++ rpsgame.js
@@ -17,42 +17,52 @@ let userScore = 0;
 
 function playRound ( user,computer ){
     const score = document.querySelector(`#currentScore`);
+    const winner = document.querySelector(`#roundWinner`);
     if ( user == "rock" ){
         if ( computer  == "rock" ){
+            winner.textContent = "Tie!";
             score.textContent = "You " + userScore + ":" + computerScore + " Computer";
         }
         if ( computer  == "paper" ){
             computerScore ++;
+            winner.textContent = "Computer wins this round!";
             score.textContent = "You " + userScore + ":" + computerScore + " Computer";
         }
         if ( computer  == "scissors" ){
             userScore ++;
+            winner.textContent = "You won this round!";
             score.textContent = "You " + userScore + ":" + computerScore + " Computer";
         }
     }
     if ( user == "paper" ){
         if ( computer  == "paper" ){
+            winner.textContent = "Tie!";
             score.textContent = "You " + userScore + ":" + computerScore + " Computer";
         }
         if ( computer  == "scissors" ){
             computerScore ++;
+            winner.textContent = "Computer wins this round!";
             score.textContent = "You " + userScore + ":" + computerScore + " Computer";
         }
         if ( computer  == "rock" ){
             userScore ++;
+            winner.textContent = "You won this round!";
             score.textContent = "You " + userScore + ":" + computerScore + " Computer";
         }
     }
     if ( user == "scissors" ){
         if ( computer  == "scissors" ){
+            winner.textContent = "Tie!";
             score.textContent = "You " + userScore + ":" + computerScore + " Computer";
         }
         if ( computer  == "rock" ){
             computerScore ++;
+            winner.textContent = "Computer wins this round!";
             score.textContent = "You " + userScore + ":" + computerScore + " Computer";
         }
         if ( computer  == "paper" ){
             userScore ++;
+            winner.textContent = "You won this round!";
             score.textContent = "You " + userScore + ":" + computerScore + " Computer";
         }
     }
